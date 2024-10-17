0672278bfd1a8be61d2dfdf5cd0c3ee9ada27f96
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-10-17 10:33:00 +0100

    Arreglamos contador al reanudar

--- app/src/main/java/org/iesharia/mytabata/CounterDown.kt
+++ app/src/main/java/org/iesharia/mytabata/CounterDown.kt
@@ -2,28 +2,32 @@ package org.iesharia.mytabata
 
 import android.os.CountDownTimer
 import android.util.Log
-import androidx.compose.runtime.Composable
 
 class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
-    private var counterState : Boolean = false
+    private var counterState: Boolean = false
+    private var tiempoRestante: Long = segundos * 1000L
 
-    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
+    private var myCounter: CountDownTimer? = null
 
-        override fun onTick(millisUntilFinished: Long) {
-            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
-        }
+    fun crearCounter(tiempo: Long) {
+        myCounter = object : CountDownTimer(tiempo, 1000) {
 
-        override fun onFinish() {
-            counterState = false
-            Log.i("dam2", "mensajito")
+            override fun onTick(millisUntilFinished: Long) {
+                tiempoRestante = millisUntilFinished
+                if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                counterState = false
+                Log.i("dam2", "mensajito")
+            }
         }
     }
 
     fun toggle() {
         Log.i("dam2", "toggle: $counterState")
-        if (this.counterState){
+        if (this.counterState) {
             this.cancel()
-
         } else {
             Log.i("dam2", "toggle: start")
             this.start()
@@ -32,11 +36,12 @@ class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
 
     fun start() {
         counterState = true
-        this.myCounter.start()
+        crearCounter(tiempoRestante)
+        myCounter?.start()
     }
 
     fun cancel() {
         counterState = false
-        this.myCounter.cancel()
+        myCounter?.cancel()
     }
 }
